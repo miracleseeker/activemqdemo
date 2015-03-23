f2663daadfce68d569f05af168d8f9a75dfc7a38
johnny <johnny.wu@dragonsoftbravo.com>
2015-03-23 18:04:43 +0800

    update

--- activitymq-demo/src/main/java/com/mike/activitymq_demo/Sender.java
+++ activitymq-demo/src/main/java/com/mike/activitymq_demo/Sender.java
@@ -1,5 +1,7 @@
 package com.mike.activitymq_demo;
 
+import java.util.UUID;
+
 import javax.jms.JMSException;
 import javax.jms.MapMessage;
 import javax.jms.Message;
@@ -7,24 +9,27 @@ import javax.jms.Session;
 
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.core.MessageCreator;
+
 public class Sender {
 	private JmsTemplate jmsTemplate;
-   public void sendInfo()
-   {
-	 jmsTemplate.send(new MessageCreator(){
-		 public Message createMessage(Session session) throws JMSException
-		 {   MapMessage message=session.createMapMessage();
-		       message.setString("firstName", "Mike--");
-		       message.setString("lastName", "Wu");		      
-			   return message;
-		 }
-		 
-	 });
-   }
-public JmsTemplate getJmsTemplate() {
-	return jmsTemplate;
-}
-public void setJmsTemplate(JmsTemplate jmsTemplate) {
-	this.jmsTemplate = jmsTemplate;
-}
+
+	public void sendInfo(final int i) {
+		jmsTemplate.send(new MessageCreator() {
+			public Message createMessage(Session session) throws JMSException {
+				MapMessage message = session.createMapMessage();
+				message.setString("firstName", UUID.randomUUID().toString());
+				message.setString("lastName", String.valueOf(i));
+				return message;
+			}
+
+		});
+	}
+
+	public JmsTemplate getJmsTemplate() {
+		return jmsTemplate;
+	}
+
+	public void setJmsTemplate(JmsTemplate jmsTemplate) {
+		this.jmsTemplate = jmsTemplate;
+	}
 }
